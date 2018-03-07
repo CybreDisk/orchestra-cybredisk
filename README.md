974fb1faf00e60a6c3065bfc336a413cf9f953d5
Andrew Zyabin [🔒] <schas002@users.noreply.github.com>
2018-03-07 14:25:08 +0400

    :sparkles: initial version of orchestra, v0.2.0
    Help, did I do this right?

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # orchestra-cybredisk
-CybreDisk build system with CybreDisk paraphernalia
+
+> CybreDisk build system with CybreDisk paraphernalia
+
+Basically, it's [Orchestra](https://github.com/CybreDisk/orchestra) dropped in a separate repository with submodules to be included in the CybreDisk.
+
+The version of Orchestra currently in use here is [0.2.0](https://github.com/CybreDisk/orchestra/releases/tag/v0.2.0).
+
+## Install
+
+```sh
+$ git clone --recurse-submodules https://github.com/CybreDisk/orchestra-cybredisk.git
+```
+
+## Usage
+
+From the orchestra-cybredisk folder run:
+
+```sh
+$ ./make.sh
+```
+
+This will create a file system in `$PREFIX`, which defaults to the `prefix` folder in the orchestra-cybredisk folder.
+
+## Maintainer
+
+- Andrew Zyabin - @schas002 - founder, CybreDisk maintainer
+
+## License
+
+[MIT](LICENSE) &copy; CybreDisk contributors
---
+++ make.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+IFS=$'\n'
+set -e
+
+VERSION='0.2.0'
+
+REPOSITORIES=`cat repositories`
+
+if [ "x$PREFIX" == "x" ] ; then
+	PREFIX="`pwd`/prefix"
+fi
+
+if [ "x$PREBUILD" == "x" ] ; then
+	PREBUILD="`pwd`/pre-build.sh"
+fi
+
+if [ "x$POSTBUILD" == "x" ] ; then
+	POSTBUILD="`pwd`/post-build.sh"
+fi
+
+rm -r "$PREFIX" 2> /dev/null || true
+mkdir "$PREFIX"
+
+echo "# CybreDisk Orchestra make.sh"
+echo "version $VERSION"
+echo "build started `date`"
+echo "prefix is $PREFIX"
+
+echo "## `date`: running pre-build actions"
+test -x "$PREBUILD" && "$PREBUILD" "$PREFIX" pre-build
+
+for i in $REPOSITORIES ; do
+	if test "$i" == "${i#\#}" ; then
+		echo "## `date`: $i"
+		echo '```'
+		pushd "$i"
+		mkdir "$PREFIX/$i"
+		make PREFIX="$PREFIX/$i"
+		popd
+		echo '```'
+	fi
+done
+
+echo "## `date`: running post-build actions"
+test -x "$POSTBUILD" && "$POSTBUILD" "$PREFIX" post-build
+
+echo "build finished `date`"
